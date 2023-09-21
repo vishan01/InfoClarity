e58dc0334dd582ca5602060780600d0a3d3eae83
Gudipati Vishnu Vardhan Reddy <vishnureddy228@gmail.com>
2023-09-21 21:21:22 +0530

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -3,7 +3,7 @@ import requests
 
 def query(payload, api):
     carrier = dict(
-        inputs=f"Input statement: '{payload}' Corrected statement is:")
+        inputs=f"'{payload}' \nGrammar Corrected statement is:")
     API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom"
     headers = {"Authorization": f"Bearer {api}"}
     response = requests.post(API_URL, headers=headers, json=carrier)
